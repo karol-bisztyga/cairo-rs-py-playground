7cfb3c002fcbe241e79d25408ee726fcdc499fb3
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-11-22 15:13:09 +0100

    fix compilers

--- scripts/initialize-compilers.sh
+++ scripts/initialize-compilers.sh
@@ -7,6 +7,8 @@ set -e
 echo "initializing new starknet..."
 
 pushd starknet_0_10_1
+poetry env info -p | xargs rm -rf || echo "skip"
+poetry env use /Users/karolbisztyga/.asdf/installs/python/3.9.14/bin/python
 CFLAGS=-I`brew --prefix gmp`/include LDFLAGS=-L`brew --prefix gmp`/lib poetry run pip install ecdsa fastecdsa sympy
 poetry run pip install cairo-lang
 popd
@@ -14,6 +16,8 @@ popd
 echo "initializing old starknet..."
 
 pushd starknet_0_9_1
+poetry env info -p | xargs rm -rf || echo "skip"
+poetry env use /Users/karolbisztyga/.asdf/installs/python/3.9.14/bin/python
 CFLAGS=-I`brew --prefix gmp`/include LDFLAGS=-L`brew --prefix gmp`/lib poetry run pip install ecdsa fastecdsa sympy
 poetry run pip install -Iv cairo-lang==v0.9.1
 popd
