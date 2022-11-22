2fbbba9c6280adfb7c7ab79c939df7a895024847
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-11-22 15:02:33 +0100

    run all contracts in dir

--- src/__main__.py
+++ src/__main__.py
@@ -1,28 +1,16 @@
-import os
+from pathlib import Path
 
 from cairo_rs_py import CairoRunner
 
-old_path = "compiled-contracts/simple-old.json"
-new_path = "compiled-contracts/simple-new.json"
-new_path = "compiled-contracts/simple-new-hacked.json"
+contracts_path = Path("compiled-contracts")
 
-if not os.path.exists(new_path):
-    raise Exception(f"required file {new_path} doesn't exist, please, compile cairo programs")
+if not contracts_path.exists():
+    raise Exception(f"required directory {contracts_path} doesn't exist, please, compile cairo programs")
 
-if not os.path.exists(old_path):
-    raise Exception(f"required file {old_path} doesn't exist, please, compile cairo programs")
-
-print("running the old program")
-with open(old_path) as file:
-    contents = file.read()
-    runner = CairoRunner(contents, "main", "all", False)
-    result = runner.cairo_run(False)
-    assert 1 == 1
-
-print("running the new program")
-with open(new_path) as file:
-    contents = file.read()
-    runner = CairoRunner(contents, "main", "all", False) # doesn't work
-    # result = runner.cairo_run(False)
+for path in contracts_path.glob("*.json"):
+    print(f"running {path}")
+    with open(path) as file:
+        runner = CairoRunner(file.read(), "main", "all", False)
+        result = runner.cairo_run(False)
 
 print("DONE")
